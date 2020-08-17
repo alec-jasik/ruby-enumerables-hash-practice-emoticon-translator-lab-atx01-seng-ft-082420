5055bec65b17c453a097255931fc21b7fdc24016
Alec Jasik <alec_jasik@yahoo.com>
2020-08-17 15:59:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,7 +12,7 @@ def load_library(file_path)
 emoticons_hash
 end
 
-def get_japanese_emoticon(file_path, emoji)
+def get_japanese_emoticon(file_path, emoticon)
 load_library("./lib/emoticons.yml")
 
 end

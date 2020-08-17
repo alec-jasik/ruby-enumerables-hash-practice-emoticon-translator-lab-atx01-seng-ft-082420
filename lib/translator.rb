fe78b422b24d457abd07e4756577af8f102525e0
Alec Jasik <alec_jasik@yahoo.com>
2020-08-17 16:02:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,11 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
 library = load_library("./lib/emoticons.yml")
-
+library.each do |meaning, idioms|
+      return idioms[:japanese] if idioms[:english] == eng_emo
+  end
+  "Sorry, that emoticon was not found"
+end
 end
 
 def get_english_meaning

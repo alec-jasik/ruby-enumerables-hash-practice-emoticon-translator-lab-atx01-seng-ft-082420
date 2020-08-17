dc4f9528abc6bff10fb852163a084db6bc69d61b
Alec Jasik <alec_jasik@yahoo.com>
2020-08-17 15:31:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ def load_library(file_path)
   emoticons.each do |name,arrays|
       emoticons_hash[name] ||= {}
       emoticons_hash[name][:english] = arrays[0]
-      frank_emo[name][:japanese] = arrays[1]
+     emoticons_hash[name][:japanese] = arrays[1]
   end
-frank_emo
+emoticons_hash
 end
